638868f1ec6e40293721ce18b9e741537e3a1562
Lee Hyohwak <crewe1746@naver.com>
2023-10-30 15:09:29 +0900

    Remove frontend related files and directories

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import AuthProvider from './context/auth'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider><App /></AuthProvider>,
-)
